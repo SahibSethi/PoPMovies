2e45c02726252dc858ff17bb5dfa38c31314c154
Sahib Sethi <sahib.sethi30@gmail.com>
2016-03-07 23:17:22 +0530

    Changes in commit

--- app/src/main/java/extras/Constant.java
+++ app/src/main/java/extras/Constant.java
@@ -7,7 +7,7 @@ public class Constant {
 
     public static final String POPULAR_MOVIES_LIST_BASE_URL = "http://api.themoviedb.org/3/discover/movie?" +
             "sort_by=popularity.desc&api_key=";
-    public static final String API_KEY = "c638d2b9a69f9f296c5b7d8add870eba";
+    public static final String API_KEY = "";// add your own key here
     public static final String MOVIE_POSTER_BASE_URL = "http://image.tmdb.org/t/p/";
     public static final String POSTER_SIZE = "w500";
     public static final String MOVIE_INTENT_KEY = "movie_intent";
